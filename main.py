e116c51963945dc2cb3437f7c8e5dfc7ab820ed9
Teng Wei <94235563+tw-ngwan@users.noreply.github.com>
2022-08-26 17:47:31 +0800

    Added direct API_KEY

--- main.py
+++ main.py
@@ -54,7 +54,8 @@ from state_user_functions import add_user_follow_up, remove_user_verification, r
     change_user_group_follow_up
 
 # Getting the API_KEY
-API_KEY = os.getenv("ATTENDANCE_TELEGRAM_API")
+API_KEY = '5323812726:AAHp0FgvJ1alHLnJunqL_DyKMZdXVVLztLM'
+# API_KEY = os.getenv("ATTENDANCE_TELEGRAM_API")
 
 # Port number to listen in for the webhook
 PORT = int(os.environ.get('PORT', 5000))
